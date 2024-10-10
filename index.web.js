ed7eaa75da0150ee5d82d47f5e8e7a08f8e617c2
hleroy360 <132720008+hleroy360@users.noreply.github.com>
2024-10-09 23:20:08 -0400

    Update index.web.js

--- index.web.js
+++ index.web.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('todos', () => App);
-AppRegistry.runApplication('todos', { rootTag: document.getElementById('root') });
+<App/>
