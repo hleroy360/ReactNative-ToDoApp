46800578e26cb7a3aac8ab969b5a92697d48386c
hleroy360 <132720008+hleroy360@users.noreply.github.com>
2024-10-12 16:19:51 -0400

    deleted:    index.web.js

--- index.web.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('todos', () => App);
-AppRegistry.runApplication('todos', { rootTag: document.getElementById('root') });
